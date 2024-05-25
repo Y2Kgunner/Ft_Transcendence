c5a0ca1e4930165d42c28efab2484e9e328c4e6a
Sir Codesalot <abdmoham.42@gmail.com>
2024-05-25 21:17:48 +0400

    auto push

--- src/frontend/js/router.js
+++ src/frontend/js/router.js
@@ -1,5 +1,5 @@
-import { setupAuthPage, logoutUser , isAuthenticated} from './auth.js';
-import { fetchUserProfile , setupAnonymizeButton , setupDeleteProfileButton, setupCloseButton, setUpVerifyDeleteOtpButton, setupUploadProfilePictureButton} from './profile.js';
+import { setupAuthPage, logoutUser, isAuthenticated } from './auth.js';
+import { fetchUserProfile, setupAnonymizeButton, setupDeleteProfileButton, setupCloseButton, setUpVerifyDeleteOtpButton, setupUploadProfilePictureButton } from './profile.js';
 import { setupTournamentPage } from './tournament.js';
 import { setupGamePage } from './game.js';
 import { setupMultiGamePage } from './multiGame.js';
@@ -13,29 +13,35 @@ class Router {
         this.setupLinks();
         await this.handleLocationChange();
     }
-    
+
     async navigate(path, { replace = false, force = false } = {}) {
-       //console.log(`Navigating to: ${path}`);
+
+        const modals = document.querySelectorAll('.modal.show');
+        modals.forEach((modal) => {
+            const modalInstance = bootstrap.Modal.getInstance(modal);
+            modalInstance.hide();
+        });
+        //console.log(`Navigating to: ${path}`);
         if (!force && this.lastPath === path && !replace) {
-           //console.log(`Already navigated to: ${path}`);
+            //console.log(`Already navigated to: ${path}`);
             return;
         }
         this.lastPath = path;
-    
+
         if (path === '/logout') {
             logoutUser();
             return this.loadRoute('/logout');
         } else if (path === '/login') {
             return this.loadRoute('pages/login.html', setupAuthPage);
         }
-    
+
         const route = this.routes[path] || this.routes['/login'];
         await this.loadRoute(route.path, route.method);
         if (!replace) {
             window.history.pushState({}, '', path);
         }
     }
-    
+
     async loadRoute(htmlPath, callback = null) {
         try {
             const response = await fetch(htmlPath);
@@ -51,8 +57,8 @@ class Router {
             console.error('Failed to load the route:', error);
         }
     }
-    
-        setupLinks() {
+
+    setupLinks() {
         document.addEventListener('click', event => {
             const routeLink = event.target.closest('[data-route]');
             if (routeLink) {
@@ -63,23 +69,23 @@ class Router {
             }
         });
     }
-    
+
     async handleLocationChange() {
         const path = window.location.pathname;
         const authStatus = await isAuthenticated();
-    
+
         if (!authStatus && path !== '/login') {
             this.navigate('/login', { replace: true });
         } else {
             this.navigate(path, { replace: true });
         }
     }
-    
+
 }
 
 const routes = {
-    '/': { path: 'pages/home.html', method: null }, 
-    '/home': { path: 'pages/home.html', method: null }, 
+    '/': { path: 'pages/home.html', method: null },
+    '/home': { path: 'pages/home.html', method: null },
     '/login': { path: 'pages/login.html', method: setupAuthPage },
     '/profile': { path: 'pages/profile.html', method: setupProfilePage },
     '/about': { path: 'pages/about.html', method: null },
@@ -100,7 +106,7 @@ async function setupProfilePage() {
         setUpVerifyDeleteOtpButton();
         setupUploadProfilePictureButton();
     } else {
-       //console.log('User is not authenticated, redirecting to login');
+        //console.log('User is not authenticated, redirecting to login');
         appRouter.navigate('/login', { replace: true });
     }
 }
