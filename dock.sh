3b562aa8721852afa9a1c4b1049a0305295e8bc4
amrokamalelsiddig <amrokamalelsiddig@gmail.com>
2024-06-10 22:57:21 +0400

    rm

--- dock.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-docker exec -it backend bash -c "python manage.py makemigrations && python manage.py migrate"
-docker exec -it backend bash -c "python manage.py collectstatic --noinput"
