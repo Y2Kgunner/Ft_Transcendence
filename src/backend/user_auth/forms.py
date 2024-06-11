ccd39842ebb8769f4392ede168a32ae730bd73e1
amrokamalelsiddig <amrokamalelsiddig@gmail.com>
2024-06-11 18:22:27 +0400

    hhjhj

--- src/backend/user_auth/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from user_auth.models import WebUser
-
-class ProfilePictureForm(forms.ModelForm):
-    class Meta:
-        model = WebUser
-        fields = ['profile_picture']  
--- src/backend/user_auth/views/test_view.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-import json
-from django.views.decorators.csrf import csrf_exempt
-
-@csrf_exempt
-def test_api(request):
-    data = {"hello" : "amoddddddddddddr"}
-    return JsonResponse(data)
-
